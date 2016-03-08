58dae51f215fbadc0b4777d4d407a55c092f8f90
Gavin Jefferies <gavin@runtime.io>
2016-03-08 10:50:00 -0800

    fix the doc menu height

--- custom-theme/js/base.js
+++ custom-theme/js/base.js
@@ -16,5 +16,26 @@ $("li.disabled a").click(function() {
     event.preventDefault();
 });
 
+function fixDocSidebar() {
+    var sidebarHeight = $("#docSidebar").height();
+    console.log('sidebar at ' + sidebarHeight);
 
+    var visibleHeight = $( window ).height() - $('#navbar').height();
+    console.log('visible at ' + visibleHeight);
 
+    // Fix the sidebar height to the visible window
+    $("#docSidebar").css("height", visibleHeight);
+
+    // If the sidebar content is larger than visible height give it scrollability.
+    if (sidebarHeight <= visibleHeight) {
+        $("#docSidebar").css("overflow", "none");
+    }
+    else {
+        $("#docSidebar").css("overflow", "scroll");
+    }
+}
+
+$( document ).ready(function() {
+    fixDocSidebar();
+    $( window ).on('resize', fixDocSidebar);
+});
