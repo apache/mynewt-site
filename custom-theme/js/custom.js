5fdba49f4947eda40b1d8e9d50fb89a4123c5347
Gavin Jefferies <gavin@runtime.io>
2016-10-10 12:30:25 -0700

    wrap long text arround images

--- custom-theme/js/custom.js
+++ custom-theme/js/custom.js
@@ -2,15 +2,17 @@ $('.show-sidebar').click(function() {
 
     $('.sidebar-container').toggle();
     $(this).toggleClass('active');
-    
+
     if ($('.sidebar-container').is(':visible')) {
-
         $(this).text('Close Docs Menu');
-
     } else {
-
         $(this).text('Docs Menu');
-
     }
-
 });
+
+// console.log("highlights: " + $(".highlight").length)
+//
+// $(".highlight").each(function(index, highlight) {
+//     console.log(highlight);
+//     console.log(highlight.innerText);
+// })
